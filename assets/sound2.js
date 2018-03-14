bb8e9541fa1c66d9ececdae8de60a8b8e04c7d4a
Maldrek <scott.salem5@gmail.com>
2018-03-14 02:02:54 -0500

    got sound to function within my files. pushing changes made to see if it will call when stored through Github

--- assets/sound2.js
+++ assets/sound2.js
@@ -1,6 +1,6 @@
 // -------------------------------- Sound Call ---------------------------------- //
 $(document).ready(function () {
-    let audio = new Audio("..assets/media/Fireplace1.mp3");
+    let audio = new Audio("../assets/media/Fireplace1.mp3");
     audio.loop = true;
     audio.volume = 0.4;
     audio.play();
