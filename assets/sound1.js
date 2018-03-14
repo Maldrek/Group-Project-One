b71ff8c05d4d6ea174534f01a3a4e688caf1fbc9
Maldrek <scott.salem5@gmail.com>
2018-03-14 01:22:59 -0500

    tried renaming folder. It is not registering in my website, though it is on Github

--- assets/sound1.js
+++ assets/sound1.js
@@ -1,6 +1,6 @@
 // -------------------------------- Sound Call ---------------------------------- //
 $(document).ready(function () {
-    let audio = new Audio("../sounds/Rain.mp3");
+    let audio = new Audio("..assets/media/Rain.mp3");
     audio.loop = true;
     audio.volume = 0.8;
     audio.play();
