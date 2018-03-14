c1532716307fb107a47609c7e0f8a3c8e8531f94
Maldrek <scott.salem5@gmail.com>
2018-03-14 18:27:18 -0500

    changed placeholder for OMDB input

--- assets/date.js
+++ assets/date.js
@@ -35,7 +35,7 @@ $("#submitI").on("click", function (event) {
                 imageR.attr("title", response.hits[i].recipe.label);
                 $("#foodInfo").append(response.hits[i].recipe.ingredientLines[j] + "<br><br>");
             };
-            $("#foodInfo").append(recipeURL);
+            $("#recipeURLDiv").append(recipeURL);
         };
         $("#foodImage").append(imageR);
     });
