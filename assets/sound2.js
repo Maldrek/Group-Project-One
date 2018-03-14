2055fa7096cba75c5dcabf137cdff6140f390e81
Maldrek <scott.salem5@gmail.com>
2018-03-14 01:19:26 -0500

    moved sounds folder to see if it will register within Github sources

--- assets/sound2.js
+++ assets/sound2.js
@@ -1,6 +1,6 @@
 // -------------------------------- Sound Call ---------------------------------- //
 $(document).ready(function () {
-    let audio = new Audio("../assets/sounds/Fireplace1.mp3");
+    let audio = new Audio("../sounds/Fireplace1.mp3");
     audio.loop = true;
     audio.volume = 0.4;
     audio.play();
